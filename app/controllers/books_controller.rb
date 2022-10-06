fdd90d60fcd50a9f4d43eeb990dec0f5caceb88c
DAIKIHIROSE <113087113+DaikiHiroseJP@users.noreply.github.com>
2022-10-06 01:56:33 +0000

    [Add]ShowEditDestroyの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,27 @@ class BooksController < ApplicationController
   end
 
   def index
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
 
   private
